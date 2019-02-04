8b26e8a2a490f2a7eb680b6e2394f9fb920ceae8
Albert Feng <alfeng6@gmail.com>
2019-02-04 15:11:23 -0800

    move pdf

--- web/viewer.js
+++ web/viewer.js
@@ -5077,7 +5077,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER
   },
   defaultUrl: {
-    value: 'tiffany_shi.pdf',
+    value: 'compressed.tracemonkey-pldi-09.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
